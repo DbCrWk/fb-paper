186402e729b397bb9b0de3ba105114bc9167841c
Dev Dabke <dev@dabke.com>
2016-08-02 23:33:38 -0700

    Fix bad find/replace side effect

--- make.sh
+++ make.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo ''Starting pdf making process (0/5)''
+echo "Starting pdf making process (0/5)"
 pdflatex main > /dev/null;
-echo ''Completed pass 1/3 of pdflatex (1/5)''
+echo "Completed pass 1/3 of pdflatex (1/5)"
 bibtex main > /dev/null;
-echo ''Completed pass 1/2 of bibtex (2/5)''
+echo "Completed pass 1/2 of bibtex (2/5)"
 pdflatex main > /dev/null;
-echo ''Completed pass 2/3 of pdflatex (3/5)''
+echo "Completed pass 2/3 of pdflatex (3/5)"
 bibtex main > /dev/null;
-echo ''Completed pass 2/2 of bibtex (4/5)''
+echo "Completed pass 2/2 of bibtex (4/5)"
 pdflatex main > /dev/null;
-echo ''Completed pass 3/3 of pdflatex (5/5)''
-echo ''Done! Pdf is at main.pdf''
+echo "Completed pass 3/3 of pdflatex (5/5)"
+echo "Done! Pdf is at main.pdf"
